2f90db0b5e5ef489a7a05759ced4d795da18693f
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-01-25 21:21:59 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-const { data } = require('./data')
+const { data } = require("./data");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
-app.get("/topRankings", async (req,res)=>{
-    const offset=getValue(req.query.offset,0);
-    const limit=getValue(req.query.limit,20) +offset;
-    const dataToSend=await data.slice(offset,limit);
-    console.log(dataToSend);
-    res.send(dataToSend);
+app.get("/topRankings", async (req, res) => {
+  const offset = getValue(req.query.offset, 0);
+  const limit = getValue(req.query.limit, 20) + offset;
+  const dataToSend = await data.slice(offset, limit);
+  console.log(dataToSend);
+  res.send(dataToSend);
 });
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 module.exports = app;
